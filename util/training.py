41567c2009da0ed8a80522fcd9e9c2ffdb259c83
Prannay Khosla <prannay.khosla@gmail.com>
2017-06-13 13:23:16 +0530

    Added code

--- util/training.py
+++ util/training.py
@@ -1,7 +1,7 @@
 import time
 from print_tweets import *
-def rank_tokens(valid_size,valid_examples, reverse_dictionary, top_k,sim):
-    for i in xrange(valid_size):
+def rank_tokens(valid_examples,valid_size, reverse_dictionary, top_k,sim):
+    for i in range(valid_size):
         valid_word = reverse_dictionary[valid_examples[i]]
         nearest = (-sim[i, :]).argsort()[1:top_k + 1]
         log_str = "Nearest to %s:" % valid_word
@@ -11,15 +11,16 @@ def rank_tokens(valid_size,valid_examples, reverse_dictionary, top_k,sim):
             word_list.append(close_word)
             log_str = "%s %s," % (log_str, close_word)
         print(log_str)
-        return word_list
+    return word_list
 def run_iteration(generator, placeholders, loss, optimizer, session, filename, batch_size, num_skips, skip_window, data, data_index):
-    data_index, batch_data = generator(data, data_index, batch_size, num_skips, skip_window)
-    feed_dict = dict(zip(placeholders, batch_data))
+    batch_data = generator(data, data_index, batch_size, num_skips, skip_window)
+    data_index = batch_data[0]
+    feed_dict = dict(zip(placeholders, batch_data[1:]))
     _,loss_val = session.run([optimizer, loss], feed_dict=feed_dict)
     return loss_val, data_index
 
 # main training model
-def train_model(session, dataset,query_similarity, query_name, word_batch_list, char_batch_list, tweet_word_holder, tweet_char_holder, generators, similarities, num_steps, placeholders,loss, optimizers, interval1, interval2, valid_size, reverse_dictionary, batch_size, num_skips, skip_window, filename,data, data_index, tweet_batch_size):
+def train_model(session, dataset,query_similarity, query_name, word_batch_list, char_batch_list, tweet_word_holder, tweet_char_holder, generators, similarities, num_steps, placeholders,loss, optimizers, interval1, interval2, valid_size, valid_examples, reverse_dictionary, batch_size, num_skips, skip_window, filename,data, data_index, tweet_batch_size):
     average_loss = list()
     count_ = 0
     g_len = len(generators)
@@ -41,6 +42,6 @@ def train_model(session, dataset,query_similarity, query_name, word_batch_list, 
         if step % interval2 == 0:
             for t in range(len(similarities)):
                 sim = similarities[t].eval()
-               rank_tokens(valid_size[i], reverse_dictionary[i], 8,sim)
-            count_ = print_tweets(dataset, query_similarity , query_name, session, word_batch_list, char_batch_list, tweet_word_holder, tweet_char_holder, count_, tweet_batch_size)
+                rank_tokens(valid_examples[t],valid_size[t], reverse_dictionary[t], 8,sim)
+            count_ = print_tweets(dataset, query_similarity , query_name, session, word_batch_list, char_batch_list, tweet_word_holder, tweet_char_holder, count_, tweet_batch_size, filename)
 
