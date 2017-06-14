e6f37e90aa81b2a82c5727d5bb418e9172a03a94
Prannay Khosla <prannay.khosla@gmail.com>
2017-06-14 12:42:08 +0530

    Expanded query

--- util/expand_query.py
+++ util/expand_query.py
@@ -1,8 +1,8 @@
 from print_tweets import *
 
-def expand_query(flag, session,holder,input_value,dataset, similarity, word_batch_dict, top_k):
+def expand_query(flag, session,holder,input_value,dataset, similarity, word_batch_dict, top_k, placeholder_1, placeholder_2):
     if not flag:
-        return []
+        return [], placeholder_1
     tweet_list = top_tweets(top_k)
     print(tweet_list)
     word_batch_list = []
@@ -13,4 +13,4 @@ def expand_query(flag, session,holder,input_value,dataset, similarity, word_batc
     l = (-sim).argsort()
     nearest = filter(lambda x: x in word_batch_list and x != 0,l)
     print(nearest[:20])
-    return nearest
+    return nearest, place_holder_2
